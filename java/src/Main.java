ab866cb80d52cbcdb767a6973cb0b0bd552ed730
KusiakMarcin <kusiakmarcin22@gmail.com>
2026-04-14 13:18:47 +0200

    TwoSums

---
+++ java/src/Main.java
@@ -0,0 +1,12 @@
+import solutions.TwoSums;
+
+import java.util.Arrays;
+
+class Main
+{
+    public static void main(String []args)
+    {
+        int[] tab = {1,2,3,4};
+        System.out.println(Arrays.toString(TwoSums.twosums(tab,7)));
+    }
+}
